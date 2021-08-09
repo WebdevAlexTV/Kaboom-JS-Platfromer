f1fcdb4a2f0ed28fa2e5a3cb48e8419178d8dff1
WebdevAlex <twitchingalex@gmail.com>
2021-08-09 22:28:07 +0200

    Optimize webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
 
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
@@ -9,6 +11,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["./resources/*"],
+    }),
     new CopyWebpackPlugin({
       patterns: [{ from: "src/resources", to: "resources" }],
     }),
